13f05d3118ea5270c006d42cf16a53c2036fa3a2
khrisantamaria <168891609+khrisantamaria@users.noreply.github.com>
2024-11-09 23:51:37 +0800

    assignment-3-factorial-calculator

--- routes/assignmentNo3_routes.js
+++ routes/assignmentNo3_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getFactorial } = require('../controller/assignmentNo3_controller');
 const router = express.Router();
 
-// POST route to calculate factorial
+
 router.get('/calculate/:number', getFactorial);
 
 module.exports = router;
